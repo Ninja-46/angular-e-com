24843d989276f083ca3abd36c3bae43cb8f9c8b5
Ninja-46 <kaushikkhara46@gmail.com>
2024-11-03 19:15:39 +0530

    first commit

---
+++ 03-frontend/angular-ecommerce/src/app/common/product.ts
@@ -0,0 +1,13 @@
+export class Product {
+    
+    id: number | undefined;
+    sku: string | undefined;
+    name: string | undefined;
+    description: string | undefined;
+    unitPrice: number | undefined;
+    imageUrl: string | undefined;
+    active: boolean | undefined;
+    unitsInStock: number | undefined;
+    dataCreated: Date | undefined;
+    lastUpdate: Date | undefined;
+}
